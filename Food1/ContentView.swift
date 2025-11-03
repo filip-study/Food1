fffb3f87da44f9012ce5b233c428d49b2d3a835a
Filip Olszak <olszak@protonmail.com>
2025-11-03 21:49:17 +0800

    Initial Commit

---
+++ Food1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food1
+//
+//  Created by Filip Olszak on 3/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food1/Food1App.swift
@@ -0,0 +1,17 @@
+//
+//  Food1App.swift
+//  Food1
+//
+//  Created by Filip Olszak on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Food1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
